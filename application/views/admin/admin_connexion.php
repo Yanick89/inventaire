3aa14166e1c6b9679f17830d58c4531abbc89719
Your Name <you@example.com>
2021-01-14 03:11:49 +0100

    vue de connexion adminisatrateur, sans gestion du responsive pour le moment

---
+++ application/views/admin/admin_connexion.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Inconsolata&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="<?= theme_url() ?>assets/css/admin_connexion.css">
+    <title>Connexion Administrateur</title>
+</head>
+<body>
+    <main>
+        <div class="contenair">
+            <div class="header">  inventaire <br> G043 </div>
+               
+           
+            <form action="">
+                <label for="identifiant">identifiant</label>
+                <input type="text" name="" id="identifiant">
+
+                <label for="mdp">mot de passe</label>
+                <input type="text" name="" id="mdp">
+                <button type="submit">se connecter</button>
+            </form>
+        </div>
+    </main>
+</body>
+</html>
