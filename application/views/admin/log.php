3f01159629ca975ada49fa5b3dc2f2551ae34290
Your Name <you@example.com>
2021-01-15 12:37:47 +0100

    vue de la liste des log

---
+++ application/views/admin/log.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Inconsolata&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="<?= theme_url() ?>assets/css/log.css">
+    <title>Log administrateur</title>
+</head>
+
+<body>
+    <main>
+        <div class="main-contenair">
+            <header>
+                <div class="header">
+                    <div class="head_first">
+                        <div class="symbole">inventaire <br> <span style="margin-left: 1.5rem;">G043</span></div>
+                        <div class="menu"><span>menu</span></div>
+                    </div>
+                    <h3>ADMINISTRATEUR</h3>
+                    <div class="head_second">
+                        <span class="btn">déconnexion <i class="fa fa-trash" aria-hidden="true"></i></span>
+                    </div>
+                </div>
+                <h3>bienvenue dans la liste des logs</h3>
+            </header>
+            <div class="table">
+                <table>
+                    <thead>
+                        <tr class="first-line-table">
+                            <th colspan="2">logs</th>
+                            <!-- <th> logs</th> -->
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>jeudi 16 /02/ 2021</td>
+                            <td>mr nzigou philber</td>
+                        </tr>
+
+                        <tr>
+                            <td>jeudi 16 /02/ 2021</td>
+                            <td>mr nze wilfried</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </main>
+</body>
+
+</html>
